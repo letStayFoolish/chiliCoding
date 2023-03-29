f7f2185c21b8148432142232afd801ed4303ed0f
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-03-29 07:30:23 +0700

    Regex validate PIN code

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 /*
 
-Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
-
-Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
-
-Note: The function accepts an integer and returns an integer.
-
-Happy Coding!
+If the function is passed a valid PIN string, return true, else return false.
 
 */
 // Type your code:
-function squareDigits(number){
-  const str = number.toString();
-  const strSqared = str.split('').map(e => {
-    return Math.pow(e, 2);
-  }).join('');
-  return Number(strSqared);
-}
+function validatePIN (pin) {
+  
+  const onlyNumbers = pin.match(pin.match(/^\d+$/));
+  const pinLength = (pin.length == 4 || pin.length == 6);
+  
+  if(onlyNumbers && pinLength) {
+    return true;
+  } return false;
+};
 // Console Log:
-console.log(squareDigits(9119)); // 811181
+console.log(validatePIN('2341'));
