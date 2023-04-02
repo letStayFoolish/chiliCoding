570559dc15081d4930d88e87f1cd34e52935f8d8
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-04-02 09:21:16 +0700

    Reverse words

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 /*
 ****************************************************************************************************************************************************
 TITLE:
-Grasshopper - Grade book
 ****************************************************************************************************************************************************
-
+Reverse words
 ****************************************************************************************************************************************************
 DESCRIPTION:
 ****************************************************************************************************************************************************
-Grade book
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
-Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+Examples
 
-Numerical Score	Letter Grade
-90 <= score <= 100	'A'
-80 <= score < 90	'B'
-70 <= score < 80	'C'
-60 <= score < 70	'D'
-0 <= score < 60	'F'
-Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
 ****************************************************************************************************************************************************
 */
 // Type your code:
-function getGrade (s1, s2, s3) {
-  // Code here
-  const averageGrade = (s1 + s2 + s3)/3;
-  return averageGrade >= 90 ? 'A' : averageGrade < 90 && averageGrade >= 80 ? 'B' : averageGrade < 80 && averageGrade >= 70 ? 'C' : averageGrade < 70 && averageGrade >= 60 ? 'D' : 'F';
-}
+function reverseWords(str) {
+  return str.split(' ').map((item) => {
+    return item.split('').reverse().join('');
+  }).join(' ');
+};
 
 // Console Log:
-console.log(getGrade(99, 67, 87));
+console.log(reverseWords('This is an example!'));
