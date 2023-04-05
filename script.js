d23feb659ea331b3d7429f8cdec36d5b219d3bf7
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-04-05 21:15:44 +0700

    Remove exclamation marks

--- script.js
+++ script.js
@@ -2,38 +2,19 @@
 ****************************************************************************************************************************************************
 TITLE:
 ****************************************************************************************************************************************************
-Sum of the first nth term of Series
+Remove exclamation marks
 ****************************************************************************************************************************************************
 DESCRIPTION:
 ****************************************************************************************************************************************************
-Task:
-
-Your task is to write a function which returns the sum of following series upto nth term(parameter).
-
-Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
-Rules:
-
-You need to round the answer to 2 decimal places and return it as String.
-
-If the given value is 0 then it should return 0.00
-
-You will only be given Natural Numbers as arguments.
-
-Examples:(Input --> Output)
-
-1 --> 1 --> "1.00"
-2 --> 1 + 1/4 --> "1.25"
-5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 ****************************************************************************************************************************************************
 */
 // Type your code:
-function SeriesSum(n) {
-  let sum = 0;
-  for(let i = 0; i < n; i++ ) {
-     sum += (1 / ((i*3) + 1));
-  }
-  return sum.toFixed(2);
+function removeExclamationMarks(s) {
+  return Array.from(s).filter((letter) => {
+    return !letter.includes('!');
+  }).join('')
 }
 
 // Console Log:
-console.log(SeriesSum(5));
+console.log(removeExclamationMarks("Hello World!"));
