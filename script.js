7ea16d1dae7590f4b3c912f7f248a9d91592cd69
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-04-13 07:38:51 +0700

    Disemvowel Trolls

--- script.js
+++ script.js
@@ -2,52 +2,33 @@
 ****************************************************************************************************************************************************
 TITLE:
 ****************************************************************************************************************************************************
-Growth of a Population
+Disemvowel Trolls
 ****************************************************************************************************************************************************
 DESCRIPTION:
 ****************************************************************************************************************************************************
-In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+Trolls are attacking your comment section!
 
-At the end of the first year there will be: 
-1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
 
-At the end of the 2nd year there will be: 
-1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+Your task is to write a function that takes a string and return a new string with all vowels removed.
 
-At the end of the 3rd year there will be:
-1141 + 1141 * 0.02 + 50 => 1213
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
-It will need 3 entire years.
-More generally given parameters:
-
-p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
-
-the function nb_year should return n number of entire years needed to get a population greater or equal to p.
-
-aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
-
-Examples:
-nb_year(1500, 5, 100, 5000) -> 15
-nb_year(1500000, 2.5, 10000, 2000000) -> 10
-Note:
-
-Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+Note: for this kata y isn't considered a vowel.
 ****************************************************************************************************************************************************
 */
 // Type your code:
-function nbYear(p0, percent, aug, p) {
+function disemvowel(str) {
+  const strVowels = ['a','e','i','o','u'];
 
-  const percentNumber = percent / 100;
-  let years = 0;
-
-  while(p0 < p) {
-    p0 = Math.floor(p0 + p0 * percentNumber + aug);
-    years ++;
-  }
-  return years;
-};
+  return str.split('').map((letter) => {
+    while(!strVowels.includes(letter.toLowerCase())) {
+      return letter;
+    }
+  }).join('')
+}
 // Console Log:
-console.log(nbYear(1500, 5, 100, 5000)); // 15
+console.log(disemvowel("This website is for losers LOL!")); // Ths wbst s fr lsrs LL!
 /****************************************************************************************************************************************************/
 
 // Using loop:
