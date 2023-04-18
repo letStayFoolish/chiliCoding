2d0a221e9c2864d9c0c460864945674a1ad796c7
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-04-18 13:31:09 +0700

    Are they the same?

--- script.js
+++ script.js
@@ -2,33 +2,53 @@
 ****************************************************************************************************************************************************
 TITLE:
 ****************************************************************************************************************************************************
-Disemvowel Trolls
+Are they the "same"?
 ****************************************************************************************************************************************************
 DESCRIPTION:
 ****************************************************************************************************************************************************
-Trolls are attacking your comment section!
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
-A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
 
-Your task is to write a function that takes a string and return a new string with all vowels removed.
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
 
-For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
 
-Note: for this kata y isn't considered a vowel.
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+Note for C
+The two arrays have the same size (> 0) given as parameter in function comp.
 ****************************************************************************************************************************************************
 */
 // Type your code:
-function disemvowel(str) {
-  const strVowels = ['a','e','i','o','u'];
-
-  return str.split('').map((letter) => {
-    while(!strVowels.includes(letter.toLowerCase())) {
-      return letter;
-    }
-  }).join('')
+function comp(array1, array2){
+  // Compare two of arrays:
+  if(array1 === null || array2 === null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.map(item => item * item).every((item, i) => item == array2[i]);
 }
 // Console Log:
-console.log(disemvowel("This website is for losers LOL!")); // Ths wbst s fr lsrs LL!
+console.log(comp(
+  [0, 121, 144, 19, 161, 19, 144, 19, 11], 
+  [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+  )); // false 
 /****************************************************************************************************************************************************/
 
 // Using loop:
