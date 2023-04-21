6799119d104d6bf89fff63f406a1b9871a302622
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-04-21 11:48:45 +0700

    Write Number in Expanded Form

--- script.js
+++ script.js
@@ -2,47 +2,33 @@
 ****************************************************************************************************************************************************
 TITLE:
 ****************************************************************************************************************************************************
-Tribonacci Sequence
+Write Number in Expanded Form
 ****************************************************************************************************************************************************
 DESCRIPTION:
 ****************************************************************************************************************************************************
-Well met with Fibonacci bigger brother, AKA Tribonacci.
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
 
-As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '7000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
 
-So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
-
-[1, 1 ,1, 3, 5, 9, 17, 31, ...]
-But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
-
-[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
-Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
-
-Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
-
-If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
-
-[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
+If you liked this kata, check out part 2!!
 ****************************************************************************************************************************************************
 */
 // Type your code:
-// function tribonacci(signature,n){
-function tribonacci(signature,n){
-  let n1 = signature[0], n2 = signature[1], n3 = signature[2], nextTerm;
-  let result = [];
-  for(let i = 0; i < n; i++) {
-    result[i] = n1;
-    nextTerm = n1 + n2 + n3;
-    n1 = n2;
-    n2 = n3;
-    n3 = nextTerm;
-  }
-  return result;
+function expandedForm(num) {
+  return num.toString()
+  .split('')
+  .reverse()
+  .map((number, i) => number*Math.pow(10, i))
+  .filter(number => number > 0)
+  .reverse()
+  .join(' + ');
 }
 // Console Log:
-console.log(tribonacci([1,1,1], 10)); // [1,1,1,3,5,9,17,31,57,105] 
-
-// console.log(tribonacci([1,1,1], 10)); // [1,1,1,3,5,9,17,31,57,105] 
+console.log(expandedForm(4056)); // '7000 + 300 + 4'
 /****************************************************************************************************************************************************/
 
 // Using loop:
