f04b30e97953853f7555e09ef3fbc9d40f752f77
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-06 09:10:13 +0700

    Two Sum

--- script.js
+++ script.js
@@ -2,46 +2,38 @@
 **********************************************************************************************************
 TITLE:
 **********************************************************************************************************
-7 kyu Anagram Detection
+6 kyu Two Sum
 **********************************************************************************************************
 DESCRIPTION:
 **********************************************************************************************************
-An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
 
-Note: anagrams are case insensitive
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
 
-Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
 
-Examples
-"foefet" is an anagram of "toffee"
+Based on: http://oj.leetcode.com/problems/two-sum/
 
-"Buckethead" is an anagram of "DeathCubeK"
+twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
 
 
 *********************************************************************************************************/
 const kataLink =
-  'https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/javascript';
+  'https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript';
 // Type your code:
 // write the function isAnagram
-function isAnagram(test, original) {
-  // check letters matching in both strings:
-  const arrTest = test.toLowerCase().split("").sort().join("")
-  const arrOriginal = original.toLowerCase().split("").sort().join("")
-
-  if(arrTest.length === arrOriginal.length) {
-    return arrTest === arrOriginal
-  } else {
-    return false
+function twoSum(nums, target) {
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j]
+    }
   }
+}
 
-};
-
-console.log(isAnagram('foefet', 'toffee')) // true
-console.log(isAnagram('DeathCubeK', 'Buckethead')) // true
-console.log(isAnagram('Twoo', 'WooT')) // true
-console.log('False results: ')
-console.log(isAnagram('dumble', 'bumble')) // false
-console.log(isAnagram('apple', 'pale')) // false
+console.log(twoSum([1, 2, 3], 4)) // [0, 2] or [2, 0] => 1 + 3 = 4 or 3 + 1 = 4
+console.log(twoSum([3, 2, 4], 6)) // [1, 2]
+console.log(twoSum([2,7,11,15], 9)) // [0, 1]
+console.log(twoSum([-370, -545, -687, -450, -450, -770, 407, -849, -531, -549, 874, 839, 10, 36, -41, -454, -589, 520, 391, -204, -926, 402, -152, -360, 497, -1000, 366, -247, -116, 879, 658, -453, 596, -94, -285, 892, -747, -193, -614, -45, -724, 64, -727, -625, 363, 782, 802, -609, -805, 789, -275, -810, -291, 359, 797, -958, 897, 673, -249, -181, 589, -981, -282, -520, -751, 809, 980, 344, 747, -230, -887, 95, 52, -693, 708], 88)) // [0, 1]
 /*********************************************************************************************************/
 
 // Using loop:
