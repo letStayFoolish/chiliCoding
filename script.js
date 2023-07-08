63afdf8abba289e0f2c4a8cd13e94af8b9b625fc
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-08 10:08:25 +0700

    Money, Money, Money

--- script.js
+++ script.js
@@ -2,69 +2,57 @@
 **********************************************************************************************************
 TITLE:
 **********************************************************************************************************
-6 kyu Give me a Diamond
+7 kyu Money, Money, Money
 **********************************************************************************************************
 DESCRIPTION:
 **********************************************************************************************************
-Jamie is a programmer, and James' girlfriend. She likes diamonds and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
 
-Task
-You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year, the new sum is re-invested.
 
-Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
-* Examples
-A size 3 diamond:
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest
 
- *
-***
- *
-...which would appear as a string of " *\n***\n *\n"
+Example:
 
-A size 5 diamond:
+  Let P be the Principal = 1000.00
+  Let I be the Interest Rate = 0.05
+  Let T be the Tax Rate = 0.18
+  Let D be the Desired Sum = 1100.00
 
-  *
- ***
-*****
- ***
-  *
-...that is:
 
-"  *\n ***\n*****\n ***\n  *\n"
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30
+Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
+
+Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+
+Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
 
 
 *********************************************************************************************************/
 const kataLink =
-  'https://www.codewars.com/kata/5503013e34137eeeaa001648/train/javascript';
+  'https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript';
 // Type your code:
-function diamond(n){
-  if(n % 2 === 0 || n < 1) {
-    return null
-  }
-  let dFirstHalf = []
-  // First half of a diamond:
-  for(let i = 0; i < (n - 1)/2; i++) {
-    dFirstHalf.push(' '.repeat(((n - 1)/2) - i) + '*'.repeat(i * 2 + 1) + '\n')
-  }
-  // Second half of a diamond:
-  return ([...dFirstHalf, "*".repeat(n) + '\n', ...dFirstHalf.reverse()].join(''))
+function calculateYears(principal, interest, tax, desired) {
 
-
-  // Solution 2:
-  // function diamond (n) {
-  //   if (n <= 0 || n % 2 === 0) return null
-  //   str = ''
-  //   for (let i = 0; i < n; i++) {
-  //     let len = Math.abs((n-2*i-1)/2)
-  //     str += ' '.repeat(len)
-  //     str += '*'.repeat(n-2*len)
-  //     str += '\n'
-  //   }
-  //   return str
-  // }
+  let years = 0
+  // If the Principal is equal to desire at the beginning, years should be 0
+  if(principal === desired) {
+    return years
+  } else {
+    for(let i = 0; principal < desired; i++) {
+      principal = Math.ceil((principal + (principal * interest) - (principal * interest) * tax))
+      years++
+    }
+  }
+  return years
 }
-
-console.log(diamond(3)) // " *\n***\n *\n"
-console.log(diamond(5)) // "  *\n ***\n*****\n ***\n  *\n"
+console.log(calculateYears(1000, 0.05, 0.18, 1100)) // 3
+console.log(calculateYears(1000,0.01625,0.18,1200)) // 14
 /*********************************************************************************************************/
 
 // Using loop:
