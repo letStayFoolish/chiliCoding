9e8edcbd7efb96596061ad272377cdba08452077
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-17 08:16:02 +0700

    Replace With Alphabet Position Using techniques: - split, map, indexOf, filter, join

--- script.js
+++ script.js
@@ -2,57 +2,34 @@
 **********************************************************************************************************
 TITLE:
 **********************************************************************************************************
-7 kyu Money, Money, Money
+6 kyu Replace With Alphabet Position
 **********************************************************************************************************
 DESCRIPTION:
 **********************************************************************************************************
-Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+DESCRIPTION:
+Welcome.
 
-The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year, the new sum is re-invested.
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
-Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+If anything in the text isn't a letter, ignore it and don't return it.
 
-Example:
+"a" = 1, "b" = 2, etc.
 
-  Let P be the Principal = 1000.00
-  Let I be the Interest Rate = 0.05
-  Let T be the Tax Rate = 0.18
-  Let D be the Desired Sum = 1100.00
-
-
-After 1st Year -->
-  P = 1041.00
-After 2nd Year -->
-  P = 1083.86
-After 3rd Year -->
-  P = 1128.30
-Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
-
-Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
-
-Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 
 *********************************************************************************************************/
 const kataLink =
-  'https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript';
+  'https://www.codewars.com/kata/546f922b54af40e1e90001da/javascript';
 // Type your code:
-function calculateYears(principal, interest, tax, desired) {
-
-  let years = 0
-  // If the Principal is equal to desire at the beginning, years should be 0
-  if(principal === desired) {
-    return years
-  } else {
-    for(let i = 0; principal < desired; i++) {
-      principal = Math.ceil((principal + (principal * interest) - (principal * interest) * tax))
-      years++
-    }
-  }
-  return years
+function alphabetPosition(text) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
+  return text.toLowerCase().split('').map((item) => alphabet.indexOf(item) + 1).filter(item => item !== 0).join(' ')
 }
-console.log(calculateYears(1000, 0.05, 0.18, 1100)) // 3
-console.log(calculateYears(1000,0.01625,0.18,1200)) // 14
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")) // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 /*********************************************************************************************************/
 
 // Using loop:
