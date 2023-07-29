6387a0e19324a196c122d0502e0de80e1ff12f6e
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-29 11:43:31 +0200

    Mumbling Using techniques: - Array map - repeat - join

--- script.js
+++ script.js
@@ -2,35 +2,28 @@
 **********************************************************************************************************
 TITLE:
 **********************************************************************************************************
-7 kyu The highest profit wins!
+7 kyu Mumbling
 **********************************************************************************************************
 DESCRIPTION:
 **********************************************************************************************************
-Story
-Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+This time no story, no theory. The examples below show you how to write function accum:
 
-Task
-Write a function that returns both the minimum and maximum number of the given list/array.
-
-Examples (Input --> Output)
-[1,2,3,4,5] --> [1,5]
-[2334454,5] --> [5,2334454]
-[1]         --> [1,1]
-Remarks
-All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 
 *********************************************************************************************************/
 const kataLink =
-  'https://www.codewars.com/kata/559590633066759614000063/train/javascript';
+  'https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript';
 // Type your code:
-function minMax(arr){
-  const minNum = Math.min(...arr)
-  const maxNum = Math.max(...arr)
-
-  return [minNum, maxNum];
+function accum(s) {
+  return [...s].map((item, index) => item.toUpperCase() + item.toLowerCase().repeat(index)).join('-')
 }
-console.log(minMax([1,2,3,4,5])) // [1, 5]
+
+console.log(accum('abcd')) // 'A-Bb-Ccc-Dddd'
 
 /*********************************************************************************************************/
 
