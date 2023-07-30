120dcc89c1938b77e427372e23c471edb6956128
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-30 09:27:24 +0200

    Lario and Muigi Pipe Problem
    Using techniques:
    - For loop

--- script.js
+++ script.js
@@ -2,28 +2,41 @@
 **********************************************************************************************************
 TITLE:
 **********************************************************************************************************
-7 kyu Mumbling
+8 kyu Lario and Muigi Pipe Problem
 **********************************************************************************************************
 DESCRIPTION:
 **********************************************************************************************************
-This time no story, no theory. The examples below show you how to write function accum:
+Issue
+Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
 
-Examples:
-accum("abcd") -> "A-Bb-Ccc-Dddd"
-accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-accum("cwAt") -> "C-Ww-Aaa-Tttt"
-The parameter of accum is a string which includes only letters from a..z and A..Z.
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+Task
+Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+Example
+Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 
 
 *********************************************************************************************************/
 const kataLink =
-  'https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript';
+  'https://www.codewars.com/kata/56b29582461215098d00000f/train/javascript';
 // Type your code:
-function accum(s) {
-  return [...s].map((item, index) => item.toUpperCase() + item.toLowerCase().repeat(index)).join('-')
+function pipeFix(numbers){
+  const first = numbers[0]
+  const last = numbers[numbers.length - 1]
+  const res = []
+
+  for (let i = first; i <= last; i++) {
+    res.push(i)
+  }
+  return res
 }
 
-console.log(accum('abcd')) // 'A-Bb-Ccc-Dddd'
+console.log(pipeFix([1,2,3,5,6,8,9]))
+console.log(pipeFix([1,2,3,12])) // [1,2,3,4,5,6,7,8,9,10,11,12]
 
 /*********************************************************************************************************/
 
